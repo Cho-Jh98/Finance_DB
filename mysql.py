77af114ce74df176c0c9e5b884b7660feebd411b
JUNWOO GWAK <69973196+GwakJunwoo@users.noreply.github.com>
2022-06-27 17:52:56 +0900

    test

---
+++ mysql.py
@@ -0,0 +1,29 @@
+import pymysql
+
+# DB information
+host = 'localhost'
+port = 3306
+user = 'user'
+password = 'New^york6'
+db = 'database1'
+charset = 'utf8'
+
+# connect
+conn = pymysql.connect(host='localhost', port=3306, user='user', password='New^york6', db='database1', charset='utf8')
+
+# cursor
+cursor = conn.cursor()
+
+# SQL
+sql = """CREATE TABLE user (
+id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+email varchar(255),
+department varchar(255)
+)
+"""
+cursor.execute(sql)
+
+rows = cursor.fetchall()
+print(rows)
+
+conn.close()
